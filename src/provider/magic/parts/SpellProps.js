f17d46c3d30ceebba30e1105b13c4293f17c83c8
Sanjeeva Chitlapalli <sanjeeva.chitlapalli@thoughtclan.com>
2025-01-23 15:01:42 +0530

    Updated to use extension elements

--- src/provider/magic/parts/SpellProps.js
+++ src/provider/magic/parts/SpellProps.js
@@ -1,13 +1,9 @@
 import { html } from 'htm/preact';
-
 import { SelectEntry, isSelectEntryEdited } from '@bpmn-io/properties-panel';
 import { useService } from 'bpmn-js-properties-panel';
-
-// import hooks from the vendored preact package
 import { useEffect, useState } from '@bpmn-io/properties-panel/preact/hooks';
 
 export default function(element) {
-
   return [
     {
       id: 'spell',
@@ -22,26 +18,39 @@ function Spell(props) {
   const { element, id } = props;
 
   const modeling = useService('modeling');
+  const moddle = useService('moddle');
   const translate = useService('translate');
   const debounce = useService('debounceInput');
 
-
   const getValue = () => {
-    return element.businessObject.spell || '';
+    const extensionElements = element.businessObject.extensionElements;
+    return extensionElements 
+      && extensionElements.values 
+      && extensionElements.values[0] 
+      && extensionElements.values[0].spellValue 
+      || '';
   };
 
-  const setValue = value => {
-    return modeling.updateProperties(element, {
-      spell: value
+  const setValue = (value) => {
+    const extensionElements = moddle.create('bpmn:ExtensionElements', {
+      values: [
+        moddle.create('magic:SpellExtension', {
+          spellValue: value
+        })
+      ]
+    });
+
+    modeling.updateProperties(element, {
+      extensionElements
     });
   };
 
   const [ spells, setSpells ] = useState([
-  'Test 1',
-  'Test 2',
-  'Test 3',
-  'Test 4',
-]);
+    'Test 1',
+    'Test 2',
+    'Test 3',
+    'Test 4',
+  ]);
 
   const getOptions = () => {
     return [
